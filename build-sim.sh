988b4330ff7c43af1f6019693f307487257ceb6a
chauhaidang1 <chauhaidang1@gmail.com>
2025-12-06 21:08:17 +0700

    update build script

--- build-sim.sh
+++ build-sim.sh
@@ -1,34 +1,5 @@
-#!/bin/bash
-# Build script for XQ Fitness iOS Simulator
-# This script should be run from the ios/ directory after pod install
+xcodebuild -resolvePackageDependencies -workspace ./XQFitness.xcworkspace -scheme XQFitness -configuration Release -derivedDataPath ./build -destination 'generic/platform=iOS Simulator'
 
-WORKSPACE="XQFitness.xcworkspace"
-SCHEME="XQFitness"
-CONFIGURATION="Release"
-DERIVED_DATA_PATH="./build"
-DESTINATION="generic/platform=iOS Simulator"
+xcodebuild -showBuildSettings -workspace ./XQFitness.xcworkspace -scheme XQFitness -configuration Release -derivedDataPath ./build -destination 'generic/platform=iOS Simulator' 2>&1
 
-# Resolve package dependencies
-xcodebuild -resolvePackageDependencies \
-  -workspace "${WORKSPACE}" \
-  -scheme "${SCHEME}" \
-  -configuration "${CONFIGURATION}" \
-  -derivedDataPath "${DERIVED_DATA_PATH}" \
-  -destination "${DESTINATION}"
-
-# Show build settings
-xcodebuild -showBuildSettings \
-  -workspace "${WORKSPACE}" \
-  -scheme "${SCHEME}" \
-  -configuration "${CONFIGURATION}" \
-  -derivedDataPath "${DERIVED_DATA_PATH}" \
-  -destination "${DESTINATION}" 2>&1
-
-# Build the project
-set -o pipefail && xcodebuild \
-  -workspace "${WORKSPACE}" \
-  -scheme "${SCHEME}" \
-  -configuration "${CONFIGURATION}" \
-  -derivedDataPath "${DERIVED_DATA_PATH}" \
-  -destination "${DESTINATION}" \
-  build
+set -o pipefail && xcodebuild -workspace ./XQFitness.xcworkspace -scheme XQFitness -configuration Release -derivedDataPath ./build -destination 'generic/platform=iOS Simulator' build
